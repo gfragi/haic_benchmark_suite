f974cf00324768739c3c163747be9101a1151b58
Giorgos Fragiadakis <gfragi@hua.gr>
2024-08-26 22:46:52 +0300

    improve the ui-ux in configuration list view

--- frontend/src/views/EvaluationConfigList.vue
+++ frontend/src/views/EvaluationConfigList.vue
@@ -5,18 +5,43 @@
         <v-col cols="12" class="text-center">
           <h1>Evaluation Configurations</h1>
         </v-col>
-        <v-btn color="primary" @click="goToNewConfig" class="ml-auto"
-          >New Configuration</v-btn
-        >
+        <v-col cols="12" class="text-right">
+          <v-text-field
+            v-model="search"
+            append-icon="mdi-magnify"
+            label="Search"
+            single-line
+            hide-details
+          ></v-text-field>
+        </v-col>
+        <v-btn color="primary" @click="goToNewConfig" class="ml-auto">
+          New Configuration
+        </v-btn>
         <v-data-table
           :headers="headers"
-          :items="configs"
+          :items="filteredConfigs"
           class="elevation-1"
-          :items-per-page="5"
+          :items-per-page="itemsPerPage"
+          :footer-props="{
+            'items-per-page-options': [
+              5,
+              10,
+              15,
+              20,
+              { text: 'All', value: -1 },
+            ],
+          }"
+          :sort-by="[sortBy]"
+          :sort-desc="[sortDesc]"
         >
           <!-- Scoped slots for customizing the actions column -->
+          <template v-slot:[`item.evaluation_status`]="{ item }">
+            <v-chip :color="getStatusColor(item.evaluation_status)" dark>
+              {{ item.evaluation_status }}
+            </v-chip>
+          </template>
           <template v-slot:[`item.actions`]="{ item }">
-            <v-btn icon color="green" text @click="evaluateConfig(item)">
+            <v-btn icon color="green" text @click="confirmEvaluateConfig(item)">
               <v-icon>mdi-play-circle</v-icon>
             </v-btn>
             <v-btn icon @click="editConfig(item)">
@@ -28,23 +53,39 @@
           </template>
         </v-data-table>
       </v-row>
+
       <v-dialog v-model="dialog" max-width="500px">
         <v-card>
           <v-card-title class="headline">Confirm Delete</v-card-title>
-          <v-card-text
-            >Are you sure you want to delete this Configuration?</v-card-text
-          >
+          <v-card-text>
+            Are you sure you want to delete this Configuration?
+          </v-card-text>
           <v-card-actions>
             <v-spacer></v-spacer>
-            <v-btn color="blue darken-1" text @click="dialog = false"
-              >Cancel</v-btn
-            >
-            <v-btn color="blue darken-1" text @click="deleteConfig"
-              >Confirm</v-btn
-            >
-            <v-btn color="blue darken-1" text @click="evaluateConfig(item)"
-              >Run Evaluation</v-btn
-            >
+            <v-btn color="blue darken-1" text @click="dialog = false">
+              Cancel
+            </v-btn>
+            <v-btn color="blue darken-1" text @click="deleteConfig">
+              Confirm
+            </v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-dialog>
+
+      <v-dialog v-model="confirmDialog" max-width="500px">
+        <v-card>
+          <v-card-title class="headline">Run Evaluation</v-card-title>
+          <v-card-text>
+            Are you sure you want to run this evaluation?
+          </v-card-text>
+          <v-card-actions>
+            <v-spacer></v-spacer>
+            <v-btn color="blue darken-1" text @click="confirmDialog = false">
+              Cancel
+            </v-btn>
+            <v-btn color="blue darken-1" text @click="runConfirmedEvaluation">
+              Confirm
+            </v-btn>
           </v-card-actions>
         </v-card>
       </v-dialog>
@@ -64,19 +105,37 @@ export default {
   data() {
     return {
       headers: [
-        { text: "Application Name", value: "application_name" },
-        { text: "AI Model Name", value: "ai_model_name" },
-        { text: "AI Model Type", value: "ai_model_type" },
-        { text: "AI Model Version", value: "ai_model_version" },
-        { text: "Description", value: "description" },
-        { text: "Evaluation Date", value: "evaluation_date" },
-        { text: "Evaluation Status", value: "evaluation_status" },
-        { text: "Actions", value: "actions", sortable: false },
+        { title: "Application Name", key: "application_name" },
+        { title: "AI Model Name", key: "ai_model_name" },
+        { title: "AI Model Type", key: "ai_model_type" },
+        { title: "AI Model Version", key: "ai_model_version" },
+        { title: "Description", key: "description" },
+        { title: "Evaluation Date Time", key: "evaluation_date" },
+        { title: "Evaluation Status", key: "evaluation_status" },
+        { title: "Actions", key: "actions", sortable: false },
       ],
       configs: [], // This will be fetched from the API
       dialog: false, // For the delete confirmation dialog
+      confirmDialog: false, // For the evaluation confirmation dialog
       configurationToDelete: null, // Temporarily store the configuration to delete
+      configToEvaluate: null, // Temporarily store the configuration to evaluate
+      search: "", // For search functionality
+      sortBy: "evaluation_date", // Default sorting column
+      sortDesc: false, // Ascending by default
+      itemsPerPage: 5, // Default items per page
     };
+  },
+  computed: {
+    filteredConfigs() {
+      if (this.search) {
+        return this.configs.filter((config) =>
+          Object.values(config).some((val) =>
+            String(val).toLowerCase().includes(this.search.toLowerCase())
+          )
+        );
+      }
+      return this.configs;
+    },
   },
   methods: {
     fetchConfigs() {
@@ -109,21 +168,39 @@ export default {
           console.error("Error deleting configuration:", error);
         });
     },
-    goToNewConfig() {
-      this.$router.push("/configuration/new"); // Ensure this route is correctly configured
+    confirmEvaluateConfig(config) {
+      this.configToEvaluate = config;
+      this.confirmDialog = true;
     },
-    evaluateConfig(config) {
+    runConfirmedEvaluation() {
+      if (!this.configToEvaluate) return;
       this.$http
-        .post(`/evaluate/${config.id}`)
+        .post(`/evaluate/${this.configToEvaluate.id}`)
         .then(() => {
-          this.$router.push(`/evaluate/${config.id}/results`);
+          this.fetchConfigs();
+          this.confirmDialog = false;
+          this.configToEvaluate = null;
         })
         .catch((error) => {
           console.error("Error running evaluation:", error);
         });
     },
+    goToNewConfig() {
+      this.$router.push("/configuration/new"); // Ensure this route is correctly configured
+    },
+    getStatusColor(status) {
+      switch (status) {
+        case "completed":
+          return "green";
+        case "pending":
+          return "orange";
+        case "failed":
+          return "red";
+        default:
+          return "grey";
+      }
+    },
   },
-
   mounted() {
     this.fetchConfigs();
   },
