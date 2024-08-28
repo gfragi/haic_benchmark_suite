6a5415fca34f4380eabcdc6f60b579d3ac7911c4
Giorgos Fragiadakis <gfragi@hua.gr>
2024-08-29 00:09:13 +0300

    WIP results frontend

--- frontend/src/views/EvaluationConfigList.vue
+++ frontend/src/views/EvaluationConfigList.vue
@@ -2,73 +2,207 @@
   <BaseLayout>
     <v-container>
       <v-row>
-        <v-col>
-          <h2>All Evaluation Results</h2>
-          <v-data-table :headers="headers" :items="results" class="elevation-1">
-            <template v-slot:[`item.evaluation_date`]="{ item }">
-              {{ new Date(item.evaluation_date).toLocaleString() }}
-            </template>
-            <template v-slot:[`item.actions`]="{ item }">
-              <v-btn color="primary" @click="viewResultDetails(item)">
-                View Details
-              </v-btn>
-            </template>
-          </v-data-table>
+        <v-col cols="12" class="text-center">
+          <h1>Evaluation Configurations</h1>
         </v-col>
+        <v-col cols="12" class="text-right">
+          <v-text-field
+            v-model="search"
+            append-icon="mdi-magnify"
+            label="Search"
+            single-line
+            hide-details
+          ></v-text-field>
+        </v-col>
+        <v-btn color="primary" @click="goToNewConfig" class="ml-auto">
+          New Configuration
+        </v-btn>
+        <v-data-table
+          :headers="headers"
+          :items="filteredConfigs"
+          class="elevation-1"
+          :items-per-page="itemsPerPage"
+          :footer-props="{
+            'items-per-page-options': [
+              5,
+              10,
+              15,
+              20,
+              { text: 'All', value: -1 },
+            ],
+          }"
+          :sort-by="[sortBy]"
+          :sort-desc="[sortDesc]"
+        >
+          <!-- Scoped slots for customizing the actions column -->
+          <template v-slot:[`item.evaluation_status`]="{ item }">
+            <v-chip :color="getStatusColor(item.evaluation_status)" dark>
+              {{ item.evaluation_status }}
+            </v-chip>
+          </template>
+          <template v-slot:[`item.actions`]="{ item }">
+            <v-btn icon color="green" text @click="confirmEvaluateConfig(item)">
+              <v-icon>mdi-play-circle</v-icon>
+            </v-btn>
+            <v-btn icon @click="editConfig(item)">
+              <v-icon>mdi-pencil</v-icon>
+            </v-btn>
+            <v-btn icon color="red" @click="confirmDeleteConfig(item)">
+              <v-icon>mdi-delete</v-icon>
+            </v-btn>
+          </template>
+        </v-data-table>
       </v-row>
+
+      <v-dialog v-model="dialog" max-width="500px">
+        <v-card>
+          <v-card-title class="headline">Confirm Delete</v-card-title>
+          <v-card-text>
+            Are you sure you want to delete this Configuration?
+          </v-card-text>
+          <v-card-actions>
+            <v-spacer></v-spacer>
+            <v-btn color="blue darken-1" text @click="dialog = false">
+              Cancel
+            </v-btn>
+            <v-btn color="blue darken-1" text @click="deleteConfig">
+              Confirm
+            </v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-dialog>
+
+      <v-dialog v-model="confirmDialog" max-width="500px">
+        <v-card>
+          <v-card-title class="headline">Run Evaluation</v-card-title>
+          <v-card-text>
+            Are you sure you want to run this evaluation?
+          </v-card-text>
+          <v-card-actions>
+            <v-spacer></v-spacer>
+            <v-btn color="blue darken-1" text @click="confirmDialog = false">
+              Cancel
+            </v-btn>
+            <v-btn color="blue darken-1" text @click="runConfirmedEvaluation">
+              Confirm
+            </v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-dialog>
     </v-container>
   </BaseLayout>
 </template>
 
 <script>
 import BaseLayout from "@/components/BaseLayout.vue";
-import evaluationService from "@/services/resultService";
+import evaluationConfigService from "@/services/configurationService";
 
 export default {
+  name: "EvaluationConfigList",
   components: {
     BaseLayout,
   },
   data() {
     return {
-      results: [],
       headers: [
-        { text: "Configuration ID", value: "configuration_id" },
-        { text: "Prediction Accuracy", value: "prediction_accuracy" },
-        { text: "Response Time", value: "response_time" },
-        { text: "Teaching Efficiency", value: "teaching_efficiency" },
-        { text: "Overall System Accuracy", value: "overall_system_accuracy" },
-        { text: "Evaluation Date", value: "evaluation_date" },
-        { text: "Actions", value: "actions", sortable: false },
+        { title: "Application Name", key: "application_name" },
+        { title: "AI Model Name", key: "ai_model_name" },
+        { title: "AI Model Type", key: "ai_model_type" },
+        { title: "AI Model Version", key: "ai_model_version" },
+        { title: "Description", key: "description" },
+        { title: "Evaluation Date Time", key: "evaluation_date" },
+        { title: "Evaluation Status", key: "evaluation_status" },
+        { title: "Actions", key: "actions", sortable: false },
       ],
+      configs: [], // This will be fetched from the API
+      dialog: false, // For the delete confirmation dialog
+      confirmDialog: false, // For the evaluation confirmation dialog
+      configurationToDelete: null, // Temporarily store the configuration to delete
+      configToEvaluate: null, // Temporarily store the configuration to evaluate
+      search: "", // For search functionality
+      sortBy: "evaluation_date", // Default sorting column
+      sortDesc: false, // Ascending by default
+      itemsPerPage: 5, // Default items per page
     };
   },
-  mounted() {
-    this.fetchResults();
+  computed: {
+    filteredConfigs() {
+      if (this.search) {
+        return this.configs.filter((config) =>
+          Object.values(config).some((val) =>
+            String(val).toLowerCase().includes(this.search.toLowerCase())
+          )
+        );
+      }
+      return this.configs;
+    },
   },
   methods: {
-    fetchResults() {
-      evaluationService
-        .getAllEvaluationResults()
+    fetchConfigs() {
+      evaluationConfigService
+        .getAllConfigs()
         .then((response) => {
-          this.results = response.data;
+          this.configs = response.data;
         })
         .catch((error) => {
-          console.error("Error fetching evaluation results:", error);
+          console.error("Error fetching configs:", error);
         });
     },
-    viewResultDetails(item) {
-      this.$router.push({
-        name: "ResultDetail",
-        params: { resultId: item.id },
-      });
+    editConfig(configuration) {
+      this.$router.push(`/configuration/edit/${configuration.id}`);
     },
+    confirmDeleteConfig(configuration) {
+      this.configurationToDelete = configuration;
+      this.dialog = true;
+    },
+    deleteConfig() {
+      if (!this.configurationToDelete) return;
+      evaluationConfigService
+        .deleteConfig(this.configurationToDelete.id)
+        .then(() => {
+          this.fetchConfigs(); // Refresh the list after deletion
+          this.dialog = false;
+          this.configurationToDelete = null; // Clear the temp storage
+        })
+        .catch((error) => {
+          console.error("Error deleting configuration:", error);
+        });
+    },
+    confirmEvaluateConfig(config) {
+      this.configToEvaluate = config;
+      this.confirmDialog = true;
+    },
+    runConfirmedEvaluation() {
+      if (!this.configToEvaluate) return;
+      evaluationConfigService
+        .runEvaluation(`${this.configToEvaluate.id}`)
+        .then(() => {
+          this.fetchConfigs();
+          this.confirmDialog = false;
+          this.configToEvaluate = null;
+        })
+        .catch((error) => {
+          console.error("Error running evaluation:", error);
+        });
+    },
+    goToNewConfig() {
+      this.$router.push("/configuration/new"); // Ensure this route is correctly configured
+    },
+    getStatusColor(status) {
+      switch (status) {
+        case "completed":
+          return "green";
+        case "pending":
+          return "orange";
+        case "failed":
+          return "red";
+        default:
+          return "grey";
+      }
+    },
+  },
+  mounted() {
+    this.fetchConfigs();
   },
 };
 </script>
-
-<style scoped>
-.subtitle-1 {
-  margin-top: 25px;
-  color: #757575;
-}
-</style>
--- frontend/src/views/EvaluationResultDetail.vue
+++ frontend/src/views/EvaluationResultDetail.vue
@@ -1,25 +1,38 @@
 <template>
   <BaseLayout>
     <v-container>
-      <v-row>
-        <v-col>
-          <h2>Evaluation Result Details</h2>
-          <v-card class="mb-4">
-            <v-card-title
-              >Result for Configuration
-              {{ result.configuration_id }}</v-card-title
-            >
+      <h2>Evaluation Result Details</h2>
+      <v-row v-if="results.length">
+        <v-col v-for="(result, index) in results" :key="index" cols="12">
+          <v-card>
+            <v-card-title>
+              Result {{ index + 1 }} - Configuration ID:
+              {{ result.configuration_id }}
+            </v-card-title>
+            <v-card-subtitle>
+              Evaluation Date:
+              {{ new Date(result.evaluation_date).toLocaleString() }}
+            </v-card-subtitle>
             <v-card-text>
-              <v-list dense>
-                <v-list-item v-for="(value, key) in result" :key="key">
-                  <v-list-item-content>
-                    <v-list-item-title>{{ key }}</v-list-item-title>
-                    <v-list-item-subtitle>{{ value }}</v-list-item-subtitle>
-                  </v-list-item-content>
-                </v-list-item>
-              </v-list>
+              <v-data-table
+                :headers="metricHeaders"
+                :items="getMetrics(result)"
+                class="elevation-1"
+              >
+                <template v-slot:[`item.metric`]="{ item }">
+                  {{ item.metric }}
+                </template>
+                <template v-slot:[`item.value`]="{ item }">
+                  {{ item.value }}
+                </template>
+              </v-data-table>
             </v-card-text>
           </v-card>
+        </v-col>
+      </v-row>
+      <v-row v-else>
+        <v-col cols="12">
+          <p>No results available.</p>
         </v-col>
       </v-row>
     </v-container>
@@ -36,7 +49,11 @@ export default {
   },
   data() {
     return {
-      result: {},
+      results: [], // Should be 'results' not 'result'
+      metricHeaders: [
+        { text: "Metric", value: "metric" },
+        { text: "Value", value: "value" },
+      ],
     };
   },
   mounted() {
@@ -46,13 +63,24 @@ export default {
     fetchResultDetail() {
       const resultId = this.$route.params.resultId;
       evaluationService
-        .getEvaluationResultDetail(resultId)
+        .getEvaluationResultsByConfig(resultId)
         .then((response) => {
-          this.result = response.data;
+          this.results = response.data;
+          console.log("Fetched Results: ", this.results); // Log the results to see their structure
         })
         .catch((error) => {
           console.error("Error fetching evaluation result detail:", error);
         });
+    },
+    getMetrics(result) {
+      // Exclude properties that are not metrics
+      const excludedKeys = ["id", "configuration_id", "evaluation_date"];
+      return Object.keys(result)
+        .filter((key) => !excludedKeys.includes(key))
+        .map((key) => ({
+          metric: key.replace(/_/g, " ").toUpperCase(), // Optional: Format the metric names
+          value: result[key],
+        }));
     },
   },
 };
