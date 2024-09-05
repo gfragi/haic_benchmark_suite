f872723d9825a237d9f3bbc38824dd5870f334b2
Giorgos Fragiadakis <gfragi@hua.gr>
2024-09-05 22:56:10 +0300

    change the logic fro storing the results to minio and sore only metadata to db. Backend tested

--- backend/app/utils/database.py
+++ backend/app/utils/database.py
@@ -2,8 +2,7 @@ from sqlalchemy import MetaData, create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-DATABASE_URL = "postgresql://postgres:CHANGEME123@localhost/bench_suite"
-
+DATABASE_URL = "postgresql://postgres:CHANGEME123@localhost/test_bench"
 metadata = MetaData()
 
 engine = create_engine(DATABASE_URL)
