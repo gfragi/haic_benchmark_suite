b9c97a93e64bafbe7e1313285eaf5ad5ea4c5b67
Giorgos Fragiadakis <gfragi@hua.gr>
2025-03-18 03:17:10 +0200

    WIP alembic migrations

--- frontend/src/services/axios.js
+++ frontend/src/services/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const apiClient = axios.create({
-  baseURL: "http://localhost:8000", // Update with your backend URL
+  baseURL: "http://localhost:8000", // Update with backend URL
   withCredentials: false,
   headers: {
     Accept: "application/json",
