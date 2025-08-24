f69411abb41bd617d84985e734f99c480afce569
Giorgos Fragiadakis <gfragi@hua.gr>
2025-08-24 22:37:36 +0300

    development fix

--- backend/main.py
+++ backend/main.py
@@ -2,8 +2,20 @@
 # alias in the root of backend
 
 
+from fastapi.middleware.cors import CORSMiddleware
 from app.main import app
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[
+        "http://localhost:8080",
+        "http://127.0.0.1:8080",
+    ],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="127.0.0.1", port=8000, reload=True)
+    uvicorn.run(app, host="127.0.0.1", port=8000, reload=True)  
